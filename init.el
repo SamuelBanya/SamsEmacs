1ab945ecdae69e0bab6228e3a409128d8d364f37
Samuel Banya <sbanya@fastmail.com>
2022-05-13 01:08:51 -0400

    Adding changes for sunrise-commander

--- init.el
+++ init.el
@@ -44,6 +44,10 @@
   (package-refresh-contents)
   (package-install 'exwm))
 
+;; Install sunrise-commander as its not part of MELPA:
+(unless (package-installed-p 'sunrise-commander)
+  (package-refresh-contents) (package-install 'sunrise-commander))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
