96872e01e54575dbaeabfcdcff4cf8f43b07641c
Samuel Banya <sbanya@fastmail.com>
2022-06-05 14:25:34 -0400

    Removing suggested revision for markdown code face, didn't work on MacOS

--- init.el
+++ init.el
@@ -66,7 +66,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 125 :width normal :foundry "1ASC" :family "hack"))))
- '(markdown-code-face ((t (:inherit consolas)))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 125 :width normal :foundry "1ASC" :family "hack")))))
 
  (org-babel-load-file "~/.emacs.d/configuration.org")
