bac2257d3d2fbc0b0efa232a4f0d6102ee42d677
Samuel Banya <sbanya@fastmail.com>
2022-06-05 13:12:00 -0400

    Changing delay to 0 for company and LSP

--- init.el
+++ init.el
@@ -52,8 +52,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(custom-enabled-themes '(spacemacs-dark))
- '(custom-enabled-themes '(morrowind-emacs-theme))
+ '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
    '("842120fcfc8dab77a1661d7575b0f35913cf849f16c6ece30ad122b6d4d793d5" "7e6995de79f9e25a0dfae2cf36edc23e9d73921da1390c4d5f0dbd0dd95f0a36" "06e0662b31a2ae8da5c6b5e9a05b25fabd1dc8dd3c3661ac194201131cafb080" "047ec205dcb5edbb94b35800110150a6e41e6cc92c0ccfb2ed25ac3df94331a6" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "69ad4071c7b2d91543fddd9030816404ff22e46f7207549319ce484e23082dee" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"))
  '(helm-minibuffer-history-key "M-p")
