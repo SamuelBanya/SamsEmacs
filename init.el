b2a901366e3080a2b72c31556549b19619dea367
Samuel Banya <sbanya@fastmail.com>
2022-06-07 14:06:25 -0400

    Cleaning up config: removing spaceline and cleaning up themes

--- init.el
+++ init.el
@@ -9,50 +9,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
-
-(unless (package-installed-p 'helm)
-  (package-refresh-contents)
-  (package-install 'helm))
-
-(unless (package-installed-p 'hydra)
-  (package-refresh-contents)
-  (package-install 'hydra))
-
-(unless (package-installed-p 'emmet-mode)
-  (package-refresh-contents)
-  (package-install 'emmet-mode))
-
-(unless (package-installed-p 'htmlize)
-  (package-refresh-contents)
-  (package-install 'htmlize))
-
-(unless (package-installed-p 'beacon)
-  (package-refresh-contents)
-  (package-install 'beacon))
-
-(unless (package-installed-p 'which-key)
-  (package-refresh-contents)
-  (package-install 'which-key))
-
-(unless (package-installed-p 'exwm)
-  (package-refresh-contents)
-  (package-install 'exwm))
-
-;; Commenting out for now:
-;; Install sunrise-commander as its not part of MELPA:
-;; (unless (package-installed-p 'sunrise-commander)
-  ;; (package-refresh-contents)
-  ;; (package-install 'sunrise-commander))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
    '("fc08f39fe01d6883bbcc1f0ebcbbf6c269923eddd76c82664d1b956d919f33d8" "842120fcfc8dab77a1661d7575b0f35913cf849f16c6ece30ad122b6d4d793d5" "7e6995de79f9e25a0dfae2cf36edc23e9d73921da1390c4d5f0dbd0dd95f0a36" "06e0662b31a2ae8da5c6b5e9a05b25fabd1dc8dd3c3661ac194201131cafb080" "047ec205dcb5edbb94b35800110150a6e41e6cc92c0ccfb2ed25ac3df94331a6" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "69ad4071c7b2d91543fddd9030816404ff22e46f7207549319ce484e23082dee" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"))
  '(helm-minibuffer-history-key "M-p")
