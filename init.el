56ae3e7246560e32f3d25c1f8d5d3beeb84b5006
Samuel Banya <sbanya@fastmail.com>
2022-06-09 11:20:22 -0400

    Re-adding Hydra to be forced to installed at first to prevent weirdness

--- init.el
+++ init.el
@@ -9,6 +9,10 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(unless (package-installed-p 'hydra)
+  (package-refresh-contents)
+  (package-install 'hydra))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
